ef69d3b5ee6eaf680af3b6f4669f96d15cd6809e
Joseph Stegall <stegalljoseph09@gmail.com>
2026-03-16 18:11:37 -0500

    Updated Google TTS Client

--- server/src/lib/googleTtsClient.js
+++ server/src/lib/googleTtsClient.js
@@ -1,5 +1,18 @@
 import textToSpeech from '@google-cloud/text-to-speech';
 
-const client = new textToSpeech.TextToSpeechClient();
+if (process.env.GOOGLE_SERVICE_ACCOUNT_BASE64) {
+  const decoded = Buffer.from(
+    process.env.GOOGLE_SERVICE_ACCOUNT_BASE64,
+    'base64'
+  ).toString();
+
+  process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON = decoded;
+}
+
+const client = new textToSpeech.TextToSpeechClient({
+  credentials: process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON
+    ? JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON)
+    : undefined,
+});
 
 export default client;
